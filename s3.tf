1104b79688ded47d0735695d28f3ee8e0a7dd2cf
pr3c10us <owo.pre.eno@gmail.com>
2022-07-23 21:11:19 +0100

    m

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 #s3 bucket
 resource "aws_s3_bucket" "logging-acadian" {
-  bucket = "acadian-logging-bucket"
+  bucket = "acadian-preciousowolabi-logging-bucket"
 }
 
 resource "aws_s3_bucket_acl" "logging_bucket_acadian_acl" {
