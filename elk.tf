d5891532315d4178299ff6d183c5fcbb5147be09
Pr3c10us <96595416+Pr3c10us@users.noreply.github.com>
2022-07-24 14:23:14 +0100

    Update elk.tf

--- elk.tf
+++ elk.tf
@@ -54,15 +54,7 @@ resource "aws_opensearch_domain" "central_logging_acadian" {
 {
   "Version": "2012-10-17",
   "Statement": [
-    {
-      "Action": "es:*",
-      "Principal": "*",
-      "Effect": "Allow",
-      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${var.domain}/*",
-      "Condition": {
-        "IpAddress": {"aws:SourceIp": ["66.193.100.22/32"]}
-      },
-      {
+   {
         "Action": [
           "es:ESHttp*"
         ],
