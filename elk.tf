8117a117924a8b9ec65cf7b46d20050721f1d227
Pr3c10us <96595416+Pr3c10us@users.noreply.github.com>
2022-07-24 14:22:24 +0100

    Update elk.tf

--- elk.tf
+++ elk.tf
@@ -49,46 +49,55 @@ resource "aws_opensearch_domain" "central_logging_acadian" {
     enforce_https       = true
     tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
-
+  
   access_policies = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
+    {
+      "Action": "es:*",
+      "Principal": "*",
+      "Effect": "Allow",
+      "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${var.domain}/*",
+      "Condition": {
+        "IpAddress": {"aws:SourceIp": ["66.193.100.22/32"]}
+      },
       {
-          "Action": [
+        "Action": [
+          "es:ESHttp*"
+        ],
+        "Principal": {
+          "AWS": "*"
+        },
+        "Effect": "Allow",
+        "Condition": {
+          {
+            "ArnEquals": {"aws:SourceArn": "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"}
+        },
+        "Condition": {"IpAddress": {"aws:SourceIp": ["0.0.0.0/0"]}
+         },
+        "Resource": [
+          "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
+          "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
+        ]
+      },
+      {
+         "Action":[
             "es:ESHttp*"
             ],
-          "Principal": {
-            "AWS": "*"
-            },
-          "Effect": "Allow",
-          "Condition": {
-            {
-              "ArnEquals": {"aws:SourceArn": "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"}
-          },
-          "Condition": {"IpAddress": {"aws:SourceIp": ["0.0.0.0/0"]}
+         "Principal": {
+           "AWS":"*"
+           },
+         "Effect": "Allow",
+         "Condition": {
+             "ArnEquals": {"aws:SourceArn": "${aws_iam_role.central_logging_acadian.arn}"}
          },
-          "Resource": [
+         "Resource": [
             "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
             "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
           ]
-      },
-      {
-            "Action":[
-               "es:ESHttp*"
-               ],
-            "Principal": {
-              "AWS":"*"
-              },
-            "Effect": "Allow",
-            "Condition": {
-                "ArnEquals": {"aws:SourceArn": "${aws_iam_role.central_logging_acadian.arn}"}
-            },
-            "Resource": [
-              "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
-              "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
-            ]
-        } 
+        }
+    }
   ]
 }
 POLICY
