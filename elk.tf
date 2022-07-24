2b6d08b8f230b2e1f3053c036f8258f62e71df24
Pr3c10us <96595416+Pr3c10us@users.noreply.github.com>
2022-07-24 21:00:03 +0100

    Update elk.tf

--- elk.tf
+++ elk.tf
@@ -89,41 +89,38 @@ CONFIG
 
 
 data "aws_iam_policy_document" "aos_access_policies" {
-  "Statement": [
+  statement : [
         {
-            "Action": [
+            actions : [
               "es:ESHttps*"
-              ],
-            "Principal": {
+            ],
+            principals : {
               "AWS": "*"
-              },
-            "Effect": "Allow",
-            "Condition": [
-              {
-                "ArnEquals": {"aws:SourceArn": "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"}
             },
-            {"IpAddress": {"aws:SourceIp": ["0.0.0.0/0"
-            ]
-           }
-           }
-           ],
-            "Resource": [
+            effects : "Allow",
+            conditions [
+              {
+                "ArnEquals": {"aws:SourceArn": "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"}},
+                {"IpAddress": {"aws:SourceIp": ["0.0.0.0/0"]}
+              }
+            ],
+            resources : [
               "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
               "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
             ]
         },
         {
-            "Action":[
+            actions :[
                "es:ESHttps*"
-               ],
-            "Principal": {
+            ],
+            principals {
               "AWS":"*"
-              },
-            "Effect": "Allow",
-            "Condition": {
+            },
+            effects : "Allow",
+            condition {
                 "ArnEquals": {"aws:SourceArn": "${aws_iam_role.central_logging_acadian.arn}"}
             },
-            "Resource": [
+            resources : [
               "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
               "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
             ]
