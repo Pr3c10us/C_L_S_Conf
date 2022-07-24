42f560111a43b0cfd392e0fecc97e3a4c8ae4eff
Pr3c10us <96595416+Pr3c10us@users.noreply.github.com>
2022-07-24 14:32:07 +0100

    Update elk.tf

--- elk.tf
+++ elk.tf
@@ -68,6 +68,18 @@ resource "aws_opensearch_domain" "central_logging_acadian" {
       "Condition": {
         "ArnEquals": {"aws:SourceArn": "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"}
       }
+    },
+     {
+      "Action": "es:ESHttp*",
+      "Principal": "*",
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
+        "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
+      ],
+      "Condition": {
+        "ArnEquals": {"aws:SourceArn": "${aws_iam_role.central_logging_acadian.arn}"}
+      },
     }
   ]
 }
