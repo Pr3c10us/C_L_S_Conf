8aec17c0f1b9a7fad12165ebd4c013039f6a7751
Pr3c10us <96595416+Pr3c10us@users.noreply.github.com>
2022-07-25 15:53:39 +0100

    Update elk.tf

--- elk.tf
+++ elk.tf
@@ -1,16 +1,15 @@
 
-resource "aws_opensearch_domain" "central_logging_acadian" {
-  domain_name           = "central-logging-testing"
+resource "aws_elasticsearch_domain" "central_logging_acadian" { 
+  domain_name           = "central-logging-testing" 
   engine_version = "OpenSearch_1.2"
 
-
   log_publishing_options {
-    cloudwatch_log_group_arn = aws_cloudwatch_log_group.central_logging_acadian_els.arn
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.example.arn
     log_type                 = "INDEX_SLOW_LOGS"
   }
 
   cluster_config {
-    instance_type = "c5.large.search"
+    instance_type = "r4.large.elasticsearch"
   }
 
   ebs_options {
@@ -19,150 +18,60 @@ resource "aws_opensearch_domain" "central_logging_acadian" {
     volume_size = 20
   }
 
-  snapshot_options {
-    automated_snapshot_start_hour = 23
-  }
-
-
-  advanced_options = {
-    "rest.action.multi.allow_explicit_index" = "true"
-  }
-
-  advanced_security_options {
-    enabled                        = false
-    internal_user_database_enabled = true
-    master_user_options {
-      master_user_name     = var.user_name
-      master_user_password = var.user_password
-    }
-  }
-
   node_to_node_encryption {
     enabled = true
   }
 
-  encrypt_at_rest {
-    enabled = true
-  }
-
   domain_endpoint_options {
-    enforce_https       = true
-    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+    enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
   }
 
-  
-  access_policies = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "es:*",
-      "Principal": "*",
-      "Effect": "Allow",
-      "Resource": [
-        "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
-        "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
-      ],
-      "Condition": {
-        "ArnEquals": {"aws:SourceArn": "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"}
-      },
-      "Condition": {
-        "IpAddress": {"aws:SourceIp": ["0.0.0.0/0"]}
+  access_policies = data.aws_iam_policy_document.os_access_policies.json
+
+}
+
+data "aws_iam_policy_document" "os_access_policies" {
+    statement {
+      effect = "Allow"
+      principals {
+        type = "AWS"
+        identifiers = "*"
       }
-    },
-    {
-      "Action":[
+      actions = [
         "es:ESHttp*"
-      ],
-      "Principal": {
-        "AWS":"*"
-      },
-      "Effect": "Allow",
-      "Condition": {
-        "ArnEquals": {"aws:SourceArn": "${aws_iam_role.central_logging_acadian.arn}"}
-      },
-      "Resource": [
-        "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
+      ]
+      resources = [
         "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
       ]
-    }  
-  ]
-}
-POLICY
-  
-  
-#   access_policies = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#       {
-#           "Action": [
-#             "es:ESHttp*"
-#             ],
-#           "Principal": {
-#             "AWS": "*"
-#             },
-#           "Effect": "Allow",
-#           "Condition": {
-#             {
-#               "ArnEquals": {"aws:SourceArn": "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"}
-#           },
-#           "Condition": {"IpAddress": {"aws:SourceIp": ["0.0.0.0/0"]}
-#          },
-#           "Resource": [
-#             "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
-#             "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
-#           ]
-#       },
-#       {
-#             "Action":[
-#                "es:ESHttp*"
-#                ],
-#             "Principal": {
-#               "AWS":"*"
-#               },
-#             "Effect": "Allow",
-#             "Condition": {
-#                 "ArnEquals": {"aws:SourceArn": "${aws_iam_role.central_logging_acadian.arn}"}
-#             },
-#             "Resource": [
-#               "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging",
-#               "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/central-logging/*"
-#             ]
-#         } 
-#   ]
-# }
-# POLICY
-
-  tags = {
-    Domain = "central_logging_acadian"
+    }
   }
+
+
+
+resource "aws_cloudwatch_log_group" "central_logging_cross_account_els" {
+    name = "central_logging_cross_account_els"
+  }
+
+resource "aws_cloudwatch_log_resource_policy" "central_logging_cross_account_els" {
+  policy_name = "central_logging_cross_account_els"
+  policy_document = data.aws_iam_policy_document.opensearch_logs.json
 }
 
-resource "aws_cloudwatch_log_group" "central_logging_acadian_els" {
-  name = "central_logging_cross_account_els"
-}
-
-resource "aws_cloudwatch_log_resource_policy" "central_logging_acadian_els" {
-  policy_name = "central_logging_acadian_els"
-
-  policy_document = <<CONFIG
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "es.amazonaws.com"
-      },
-      "Action": [
-        "logs:PutLogEvents",
-        "logs:PutLogEventsBatch",
-        "logs:CreateLogStream"
-      ],
-      "Resource": "arn:aws:logs:*"
-    }   
- ]
-}
-CONFIG
+data "aws_iam_policy_document" "opensearch_logs" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = ["es.amazonaws.com"]
+    }
+    actions = [
+      "logs:PutLogEvents",
+      "logs:PutLogEventsBatch",
+      "logs:CreateLogStream",
+    ]
+    resources = [
+      "arn:aws:logs:*"
+    ]
+  }
 }
