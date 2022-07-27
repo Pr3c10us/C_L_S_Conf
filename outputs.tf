1732e569c15f3517fb723c0836eb5ed0c7ad41c0
Pr3c10us <96595416+Pr3c10us@users.noreply.github.com>
2022-07-27 14:25:05 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "es_endpoint" {
-  value = "${aws_opensearch_domain.central_logging_acadian.endpoint}"
-}
+# output "es_endpoint" {
+#   value = "${aws_opensearch_domain.central_logging_acadian.endpoint}"
+# }
 
 output "es_arn" {
   value = "${aws_opensearch_domain.central_logging_acadian.arn}"
